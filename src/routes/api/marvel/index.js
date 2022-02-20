6619f4ea9a164816f7bd219bd6bfae2d6a5f6d15
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2022-02-20 00:24:24 -0700

    test

--- src/routes/api/marvel/index.js
+++ src/routes/api/marvel/index.js
@@ -1,16 +1,16 @@
 import md5 from 'crypto-js/md5.js';
 
-import { config } from './marvelConfig';
+// import { config } from './marvelConfig';
 
 // import variables from '$lib/env';
 // console.log(variables);
 
 export const get = async () => {
 	const ts = new Date().getTime();
-	const privateKEY = config.marvelKEYSecret;
-	const key = config.marvelKey;
-	// const privateKEY = import.meta.env.PRIVATE_KEY;
-	// const key = import.meta.env.PUBLIC_KEY;
+	// const privateKEY = config.marvelKEYSecret;
+	// const key = config.marvelKey;
+	const privateKEY = import.meta.env.PRIVATE_KEY;
+	const key = import.meta.env.PUBLIC_KEY;
 	// const privateKEY = import.meta.env.VITE_PRIVATE_KEY;
 	// const key = import.meta.env.VITE_PUBLIC_KEY;
 
