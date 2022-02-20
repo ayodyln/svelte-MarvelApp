63acb8274dc85f14037b62c3f8970657e55c4c2f
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2022-02-19 23:36:30 -0700

    test

--- src/routes/api/marvel/index.js
+++ src/routes/api/marvel/index.js
@@ -1,18 +1,18 @@
-import md5 from "crypto-js/md5.js";
+import md5 from 'crypto-js/md5.js';
 
-// import { config } from './marvelConfig';
+import { config } from './marvelConfig';
 
 // import variables from '$lib/env';
 // console.log(variables);
 
 export const get = async () => {
 	const ts = new Date().getTime();
-	// const privateKEY = config.marvelKEYSecret;
-	// const key = config.marvelKey;
+	const privateKEY = config.marvelKEYSecret;
+	const key = config.marvelKey;
 	// const privateKEY = import.meta.env.PRIVATE_KEY;
 	// const key = import.meta.env.PUBLIC_KEY;
-	const privateKEY = import.meta.env.VITE_PRIVATE_KEY;
-	const key = import.meta.env.VITE_PUBLIC_KEY;
+	// const privateKEY = import.meta.env.VITE_PRIVATE_KEY;
+	// const key = import.meta.env.VITE_PUBLIC_KEY;
 
 	const hash = md5(ts + privateKEY + key);
 
--- src/routes/api/marvel/marvelConfig.js
+++ src/routes/api/marvel/marvelConfig.js
@@ -1,4 +1,4 @@
 export let config = {
-	marvelKey: `8a51610a0e82d6d4414b79e79a8bf29a`,
-	marvelKEYSecret: `9eb2f9160d8e311cb9966e6f4823e8a9ffce431e`
+	marvelKey: import.meta.env.VITE_PUBLIC_KEY,
+	marvelKEYSecret: import.meta.env.VITE_PRIVATE_KEY
 };
